86e150144e5cc4874dc224a5c6789d702d3603d6
getgameenviroment <dioresp12@gmail.com>
2025-01-18 21:21:24 +0200

    Add files via upload

---
+++ HTScript.lua
@@ -0,0 +1,7 @@
+Goals for the script currently:
+
+- Add better name esp
+- Add more features to name esp (changing font, name size, etc)
+- Maybe re-make the UI (dont know about this one yet)
+- Add UI Color changer
+- Add a blatant tab for alts (spinbot, walkspeed, target strafe, auto-tp to last spot, save location, etc.)
